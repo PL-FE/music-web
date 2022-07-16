a2d5ef290e86d55403c11be0daadd55b46f4873f
pengliang <624151713@qq.com>
2022-07-16 15:47:19 +0800

    # 简单优化进度条

--- src/views/ControlBar.vue
+++ src/views/ControlBar.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="controbar-container">
-        <el-slider class="slider" :model-value="progress" @input="progressChange" size="small" />
+        <el-slider class="slider" :model-value="progress" @input="progressChange" size="small"
+            :format-tooltip="formatTooltip" />
         <div class="controller-main left">
             <svg-icon @click="hanlderPrevious" class="icon-svg" iconName="icon-previous"></svg-icon>
             <svg-icon @click="hanlderpause" class="icon-svg main" iconName="icon-pause" v-if="musicStore.playing">
@@ -30,76 +31,113 @@ import { watchEffect, ref, nextTick, reactive, computed } from 'vue';
 import { getSongDounloadUrl } from '@/api/music';
 import SongItem from '@/components/SongItem.vue';
 import { millisecondToTime } from '@/utils/index'
-
+// TODO: 解决缓存进度条的显示
+// TODO: 解决跳转未缓存到的时间，而暂停
+// TODO: 进度条需要重写，当前是简单版本
 const musciArrts = reactive({
     mp3Url: '',
     duration: 0,
     currentTime: 0,
     curtimeSlider: 0
 })
-const progress = computed(() => {
-    if (!musciArrts.currentTime) return 0
-    return musciArrts.currentTime / (musciArrts.duration / 1000) * 100
-})
-const progressChange = (val: number) => {
-    console.dir(audioRef.value);
-
-    if (audioRef.value) {
-        const time = val * (musciArrts.duration / 1000) / 100
-        audioRef.value.currentTime = time;
-    }
-}
 const musicStore = defineMusicStore()
 const audioRef = ref<HTMLAudioElement>()
-watchEffect(async () => {
-    if (musicStore.curSong) {
-        const id = musicStore.curSong.id
-        try {
-            const songData: any = await getSongDounloadUrl(<string>id)
-            musciArrts.mp3Url = songData.url
-        } catch (error) {
-            musicStore.playing = false
+const {
+    progress,
+    progressChange,
+    customChangeProgress,
+    formatTooltip,
+} = useProgress()
+
+const { hanlderPlay, hanlderpause, hanlderNext, hanlderPrevious, handleTimeupdate } = useAudio(customChangeProgress.value)
+
+function useAudio(customChangeProgress: boolean) {
+    watchEffect(async () => {
+        if (musicStore.curSong) {
+            const id = musicStore.curSong.id
+            try {
+                const songData: any = await getSongDounloadUrl(<string>id)
+                musciArrts.mp3Url = songData.url
+            } catch (error) {
+                musicStore.playing = false
+            }
+            musciArrts.duration = musicStore.curSong.song.duration
         }
-        musciArrts.duration = musicStore.curSong.song.duration
+    })
+    watchEffect(() => {
+        if (musciArrts.mp3Url) {
+            if (musicStore.playing) {
+                nextTick(() => {
+                    audioRef.value?.play()
+                })
+            } else {
+                nextTick(() => {
+                    audioRef.value?.pause()
+                })
+            }
+        }
+    })
+
+    // 监听播放事件
+    const hanlderPlay = () => {
+        musicStore.playing = true
     }
-})
-watchEffect(() => {
-    if (musciArrts.mp3Url) {
-        if (musicStore.playing) {
-            nextTick(() => {
-                audioRef.value?.play()
-            })
-        } else {
-            nextTick(() => {
-                audioRef.value?.pause()
-            })
+    // 监听暂停事件
+    const hanlderpause = () => {
+        musicStore.playing = false
+    }
+    // 下一首
+    const hanlderNext = () => {
+        musicStore.nextSong()
+    }
+    // 上一首
+    const hanlderPrevious = () => {
+        musicStore.preSong()
+    }
+    // 播放位置被改变
+    const handleTimeupdate = () => {
+        if (!customChangeProgress) {
+            musciArrts.currentTime = <number>audioRef.value?.currentTime
+            musicStore.currentTime = musciArrts.currentTime
         }
     }
-})
-// 监听播放事件
-const hanlderPlay = () => {
-    musicStore.playing = true
-}
-// 监听暂停事件
-const hanlderpause = () => {
-    musicStore.playing = false
-}
-// 下一首
-const hanlderNext = () => {
-    musicStore.nextSong()
-}
-// 上一首
-const hanlderPrevious = () => {
-    musicStore.preSong()
-}
-// 播放位置被改变
-const handleTimeupdate = () => {
-    musciArrts.currentTime = <number>audioRef.value?.currentTime
-    musicStore.currentTime = musciArrts.currentTime
+
+    return { hanlderPlay, hanlderpause, hanlderNext, hanlderPrevious, handleTimeupdate }
 }
 
 function useAudioGetCurtime(currentTime: number) {
     return millisecondToTime(Math.floor(currentTime * 1000))
+}
+
+function useProgress() {
+    const progress = computed(() => {
+        if (!musciArrts.currentTime) return 0
+        console.log(musciArrts.currentTime / (musciArrts.duration / 1000) * 100);
+        return musciArrts.currentTime / (musciArrts.duration / 1000) * 100
+    })
+    let customChangeProgress = ref(false)
+    let customChangeProgressTimer: any = null
+    // 进度条 input 事件
+    const progressChange = (val: any) => {
+        clearTimeout(customChangeProgressTimer)
+        customChangeProgressTimer = setTimeout(() => {
+            customChangeProgress.value = false
+        }, 500)
+        customChangeProgress.value = true
+        if (audioRef.value) {
+            const time = val * (musciArrts.duration / 1000) / 100
+            audioRef.value.currentTime = time;
+        }
+    }
+    const formatTooltip = (value: number) => {
+        return millisecondToTime(value * (musciArrts.duration / 100))
+    }
+    return {
+        progress,
+        progressChange,
+        customChangeProgress,
+        formatTooltip,
+    }
 }
 </script>
 
